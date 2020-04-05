aab8931807288874f73ac2aa800948a9af004aee
saheen1998 <47032114+saheen1998@users.noreply.github.com>
2020-04-04 20:11:24 -0500

    Commit

--- app/src/main/java/com/example/lab7/App.java
+++ app/src/main/java/com/example/lab7/App.java
@@ -9,8 +9,8 @@ public class App extends Application {
     public static final String CHANNEL_2_ID = "channel2";
 
     @Override
-    public void OnCreate() {
-        super.OnCreate();
+    public void onCreate() {
+        super.onCreate();
 
         createNotificationChannels();
     }
